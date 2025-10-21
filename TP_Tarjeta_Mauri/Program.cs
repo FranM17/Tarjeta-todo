3ef47545ba68dcf810cb3eec2232d6a07d52ac3e
Franco Mondino <franmondi17@gmail.com>
2025-10-21 13:08:15 -0300

    todos los archivos del repo

---
+++ TP_Tarjeta_Mauri/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TP_Tarjeta_Mauri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+
+
+
+        }
+    }
+}
